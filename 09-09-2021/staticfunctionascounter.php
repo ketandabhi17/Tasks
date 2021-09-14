48c324c3dd37a62fb81a49094b3ffaf13afb8e93
ketandabhi17 <89981844+ketandabhi17@users.noreply.github.com>
2021-09-14 11:18:14 +0530

    Add files via upload

---
+++ 09-09-2021/staticfunctionascounter.php
@@ -0,0 +1,14 @@
+<?php
+class solution {
+	static $count;
+	public static function getCount() {
+		return self::$count++;
+	}
+}
+solution::$count = 1;
+for($i = 0; $i < 5; ++$i) {
+	echo 'The next value is: '.
+	solution::getCount() . "\n";
+}
+
+?>
