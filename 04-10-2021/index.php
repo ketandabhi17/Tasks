2b074f667c4a57eaed34db45f6fd025049f0e245
ketandabhi17 <89981844+ketandabhi17@users.noreply.github.com>
2021-10-05 10:17:29 +0530

    Add files via upload

---
+++ 04-10-2021/index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="next.php" method="post">
+Name: <input type="text" name="name">
+Email: <input type="text" name="email">
+<input type="button">
+</form>
+
+</body>
+</html> 
